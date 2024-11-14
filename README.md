0cb8c0103dd71f6fc4a9a1c34c3aa9ed5f073f38
Ahmed Marzook <m4marzook@gmail.com>
2024-11-14 19:29:18 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Weather-Bridge
+ A modern Weather API wrapper service providing seamless access to weather data through a unified interface. WeatherBridge acts as an elegant connector between your applications and various weather data providers.
---
+++ src/main/java/com/kaizenflow/weatherbridge/WeatherbridgeApplication.java
@@ -0,0 +1,13 @@
+package com.kaizenflow.weatherbridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherbridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherbridgeApplication.class, args);
+	}
+
+}
