d1423a6543abf620de6b5afccfb0cd78cdee40be
Ahmed Marzook <m4marzook@gmail.com>
2024-11-26 11:27:40 +0000

    EC2 instance to correct subnet

--- terraform/ec2-instances.tf
+++ terraform/ec2-instances.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "app_server" {
   ami           = var.instance_ami
   instance_type = var.instance_type
-  subnet_id     = aws_subnet.public.id
+  subnet_id     = aws_subnet.public_a.id
   key_name      = "weather-bridge"
 
   vpc_security_group_ids = [aws_security_group.weather_bridge_sg.id]
